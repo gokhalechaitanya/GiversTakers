30615c7261d3188652c13a629a75f8baf90d3529
marcus frean <marcus@ecs.vuw.ac.nz>
2015-03-10 17:40:13 +1300

    getting it going in pair test

--- code/givers.py
+++ code/givers.py
@@ -8,27 +8,30 @@ Marcus Frean.
 """
 import numpy as np
 import numpy.random as rng
-import pylab as pl
 import networkx as nx
 from network_pic import *
 np.set_printoptions(precision=2)
 
-commodities = ['apples','shoes','wheat']        
-      
-#%% ----------- start of Trader class definition ------------------------------------------
-class Trader:
-    def __init__(self, name = ''):
+
+
+#%% ----------- start of Giver class definition ------------------------------------------
+class Giver:
+    def __init__(self, world, name, start_counts=None):
+        self.world = world # the world this agent lives within.        
         self.name = name
-        #self.count =  rng.randint(1,10, size=len(commodities))
-        #vals = rng.dirichlet(3.0*np.ones(len(commodities)))
+        #self.count =  rng.randint(1,10, size=len(    self.world.commodities))
+        #vals = rng.dirichlet(3.0*np.ones(len(self.world.commodities)))
         # I like this as I can do inheritance of "similar" values
         # easily: use scaled up "parent" vals as the alpha!
-        self.count = {x: rng.randint(0,5)  for x in commodities}
+        if start_counts is None:
+            self.count = {x: rng.randint(0,5)  for x in self.world.commodities}            
+        else:
+            self.count = start_counts        
+            
         self.neighbours = []
         # keys for the following will be the neighbours for the following dictionaries
         self.given_val = {}       
         self.recd_val = {}
-        self.num_trades = {} 
         self.W0 = rng.normal()  
         # strength of tendency to PASS (avoid gifting at all)
 
@@ -41,21 +44,27 @@ class Trader:
         # (a +ve W2 means agent prefers to trade when it is "in the red")
 
         self.utility_trail = [] # just a record of utility over a season
+        self.counts_trail = {x: []  for x in self.world.commodities}
+        # that is just a record for each commodity over a season
+        
+        self.append_trail()
 
     def __str__(self):
         return self.name
 
-    def _get_utility(self):
+    def get_utility(self):
         """
-        return the utility to this agent of having the suggested commodity counts
+        return the utility to this agent of having the current commodity counts
         """
         util = 0.0
-        for c in commodities:
+        for c in self.world.commodities:
             util += np.log(1 + self.count[c])  # for example....
         return util 
 
-    def append_utility_trail(self):
-        self.utility_trail.append(self._get_utility())
+    def append_trail(self):
+        self.utility_trail.append(self.get_utility())
+        for x in self.world.commodities:
+            self.counts_trail[x].append(self.count[x])
                 
     def add_neighbour(self, new_neighbour):
         if (new_neighbour in self.neighbours) or (new_neighbour.name == self.name):
@@ -64,24 +73,28 @@ class Trader:
             self.neighbours.append(new_neighbour)
             self.given_val[new_neighbour] = 0.0
             self.recd_val[new_neighbour]  = 0.0
-            self.num_trades[new_neighbour] = 0
             return True
 
     def set_count(self, commodity, i): 
         self.count[commodity] = i
 
+    def set_weights(self, w0, w1, w2): 
+        self.W0 = w0
+        self.W1 = w1
+        self.W2 = w2
 
     def display(self):
         print '--------------------------------------------'
-        print 'Trader ', self.name
-        print 'W0,W1,W2 : %.2f, %.2f, %.2f' %(self.W0, self.W1, self.W2)
+        print 'Giver ', self.name
+        print '\t neighbours: ' % (self.neighbours)
+        print '\t W0,W1,W2 : %.2f, %.2f, %.2f' %(self.W0, self.W1, self.W2)
         for nb in self.neighbours:
-            print 'On %4d trades: gave %.2f, recd %.2f \t with %s' % (self.num_trades[nb], self.given_val[nb], self.recd_val[nb], nb)
-        for x in commodities:
-            print '%8s: %3d' % (x, self.count[x])
-        print 'utility = %.2f' %(self._get_utility())
+            print '\t with %5s: gave %.2f, recd %.2f' % (nb, self.given_val[nb], self.recd_val[nb])
+        for x in self.world.commodities:
+            print '\t %8s: %3d' % (x, self.count[x])
+        print '\t utility = %.2f' %(self.get_utility())
         
-    def do_one_gift(self, verbose=False):
+    def do_one_gift(self, temperature = 1.0, verbose=False):
         """Consider giving one of a commodity to one neighbour. Evaluate
         willingness to do this for all commodities and all neighbours,
         including a do-nothing option.
@@ -92,120 +105,141 @@ class Trader:
         if verbose: self.display()
         
         #%% first we decide what to do
-        drive = np.zeros(shape=(len(self.neighbours), len(commodities)), dtype=float)
-        base_utility = self._get_utility()   
+        #print 'CHECK: %d nbrs and %d commods' % (len(self.neighbours), len(self.world.commodities))
+        drive = np.zeros(shape=(len(self.neighbours), len(self.world.commodities)), dtype=float)
+        base_utility = self.get_utility()   
         for j,nb in enumerate(self.neighbours):
-            for i,c in enumerate(commodities):
+            for i,c in enumerate(self.world.commodities):
                 # eval the willingness to give this c to this nb.
                 # Thought experiment: "what if I gave c away to nb?"
                 if self.count[c] > 0:
                     self.count[c] -= 1 
-                    loss = self._get_utility() - base_utility
+                    loss = self.get_utility() - base_utility
                     self.count[c] += 1  # cos it's only a thought expt! 
                 else:
-                    loss = -np.Inf
+                    loss = -99.0
                 drive[j,i] = self.W1 * loss + self.W2 * (self.recd_val[nb] - self.given_val[nb])
+        if verbose: 
+            print 'drive is ', drive
+        drive_noaction = self.W0
+        
+        willingness = np.exp(drive/temperature)
+        unwillingness = np.exp(drive_noaction/temperature)
+        total_willingness = np.sum(np.ravel(willingness))
 
-
-        willingness = np.exp(drive)
-        total_willing = np.sum(np.ravel(willingness))
-        total_unwilling = np.exp(self.W0)
         # First decision: do nothing?
-        if total_willing > total_unwilling:
-            willingness = willingness / np.sum(np.ravel(willingness))
+        if rng.random() < unwillingness / (unwillingness + total_willingness):
+            # do nothing
+            if verbose: print '#### best to PASS: %.2f better than %.2f' %(unwillingness, total_willingness)
+        else:
+            prob = willingness/np.sum(willingness)
+            cumulativeProb = np.cumsum(prob)
             if verbose: 
-                print '#### %s considers gifting ' % (self.name), commodities
+                print '#### %s considers gifting ' % (self.name), self.world.commodities
                 for j,nb in enumerate(self.neighbours):
-                    print '#### to %5s: '%(nb), willingness[j]
-            best_nb_index, best_commod_index = np.unravel_index(willingness.argmax(), willingness.shape)
-            best_nb = self.neighbours[best_nb_index]
-            best_commod = commodities[best_commod_index]
-            if verbose: print '#### Best option: give %s to %s' % (best_commod, best_nb)
+                    print '#### to %5s (drive) :' %(nb),
+                    print drive[j,:]
+                    print '#### to %5s (cumPr) :' %(nb),
+                    print prob #cumulativeProb.reshape(willingness.shape)[j,:]
+            # choose an action with probability proportional to the normalised will.
+            r = rng.random()
+            i = np.sum(r > cumulativeProb)
+            chosen_nbr_index, chosen_commod_index = np.unravel_index(i, willingness.shape)
+            nbr = self.neighbours[chosen_nbr_index]
+            commod = self.world.commodities[chosen_commod_index]
+            if verbose: print '#### (%.2f) Choice: give %s to %s' % (r, commod, nbr)
                 
-            #%% So do it! Then DO IT, ie. update count, given_val, recd_val, num_trades for both self, and the receiver.
+            #%% So do it! Then DO IT, ie. update count, given_val, recd_val, for both self, and the receiver.
             # Q. how to decide whether to just do nothing though?
-            if verbose: best_nb.display()
-            self.give(best_commod, best_nb)
+            self.give(commod, nbr)
+
             if verbose: 
                 self.display()
-                best_nb.display()
-        else:  
-            # Agent prefers to PASS regarding gifting, this round
-            if verbose: print '#### best to PASS: %.2f better than %.2f' %(total_unwilling, total_willing)
+                nbr.display()
 
-    def give(self, commodity, trader):
+    def give(self, commodity, recipient):
         """ Give a specific commodity to a specific trader """
         if self.count[commodity] == 0:
             return
             
-        base_utility = self._get_utility()   
+        base_utility = self.get_utility()   
         self.count[commodity]    -= 1
-        lost = self._get_utility() - base_utility
-        self.given_val[trader] -= lost
-        self.num_trades[trader] += 1
+        lost = self.get_utility() - base_utility
+        self.given_val[recipient] -= lost
     
-        base_utility = trader._get_utility()   
-        trader.count[commodity] += 1
-        gained = trader._get_utility() - base_utility
-        trader.recd_val[self] += gained
-        trader.num_trades[self] += 1
+        base_utility = recipient.get_utility()   
+        recipient.count[commodity] += 1
+        gained = recipient.get_utility() - base_utility
+        recipient.recd_val[self] += gained
         
-# ----------- end of Trader class definition ------------------------------------------
+# ----------- end of Giver class definition ------------------------------------------
+# ----------- start of World class definition ------------------------------------------
+class World:
+    def __init__(self, commodities=[]):
+        self.commodities = commodities
+        self.givers = []
+        
+    def add_node(self, g1, edges=[]):
+        """ add a new node to the world, and (optionally) connect that node up """
+        self.givers.append(g1)
+        for g2 in edges:
+            g1.add_neighbour(g2)
+            g2.add_neighbour(g1)
+        
+    def do_one_season(self, verbose=False):
+        """Zillions of trades happen. Agents accumulate rewards. 
+    
+        Should we have a fixed number of trades? Should rewards be grand sum,
+        or on-the-fly exponentially weighted moving average?
+        """
+        # First, reset the initial amounts of stuff - e.g. reflecting stochasticity in the environment.
+        for tr in self.givers:
+            tr.utility_trail = [] # reset trail to blank - it's a new season.
+            tr.count = {x: rng.randint(0,5)  for x in tr.world.commodities}
+    
+        # Second, do a bunch of rounds. In each round everyone gets to give if they want.
+        for t in range(10):  # could probably be bigger!
+            # do one round of all traders, in a random order
+            for i in rng.permutation(len(self.givers)):
+                tr = self.givers[i]
+                tr.do_one_gift(1.0, verbose)
+                tr.append_trail()
+    
+    def adapt_all_traders():
+        """Everyone reconsiders their position, given their relative
+        performance.  For example, perhaps everyone adopts the values and
+        strategy (?) of someone else who is doing better. BUT HEY that
+        won't do: if I'm in an apple-rich region do I suddenly want to
+        value shoes? Sure, IF I DO then I might as well grab the
+        shoe-guy's behavioural strategy too. But this seems to defy
+        locality.
+        """
+
+# ----------- end of World class definition ------------------------------------------
+
 
 #%% Other methods - the global stuff
 
-def do_one_season(traders, verbose=False):
-    """Zillions of trades happen. Agents accumulate rewards. 
-
-    Should we have a fixed number of trades? Should rewards be grand sum,
-    or on-the-fly exponentially weighted moving average?
-    """
-    # First, reset the initial amounts of stuff - e.g. reflecting stochasticity in the environment.
-    for tr in traders:
-        tr.utility_trail = [] # reset trail to blank - it's a new season.
-        tr.count = {x: rng.randint(0,5)  for x in commodities}
-        tr.append_utility_trail()
-
-    # Second, do a bunch of rounds. In each round everyone gets to give if they want.
-    for t in range(10):  # could probably be bigger!
-        # do one round of all traders, in a random order
-        for i in rng.permutation(len(traders)):
-            tr = traders[i]
-            tr.do_one_gift(verbose)
-            tr.append_utility_trail()
-
-def adapt_all_traders():
-    """Everyone reconsiders their position, given their relative
-    performance.  For example, perhaps everyone adopts the values and
-    strategy (?) of someone else who is doing better. BUT HEY that
-    won't do: if I'm in an apple-rich region do I suddenly want to
-    value shoes? Sure, IF I DO then I might as well grab the
-    shoe-guy's behavioural strategy too. But this seems to defy
-    locality.
-
-    """
 
 
 if __name__ == '__main__':
 
     # Define the trader network
-    names = ['Bulbulia', 'Yoyo', 'Bianca', 'Bob'] #, 'Serena', 'Kurt', 'Shaun', 'Shona', 'Grant', 'Bill', 'Shorty', 'Kim', 'Tyler']
-    traders = []
+    world = World(['apples','shoes','wheat'])
+
+    names = ['Bulbulia', 'Yoyo', 'Bianca', 'Mark', 'Serena', 'Kurt', 'Shaun', 'Shona', 'Grant', 'Bill', 'Shorty', 'Kim', 'Tyler']
+    
     for name in names:
-        tr = Trader(name)
-        traders.append(tr)
+        world.add_node(Giver(world, name), edges=[])
     # link them up somehow...
-    edge_ends_list = []
-    edge_labels = []
-    for tr1 in traders:
+    min_num_links = 2
+    for tr1 in world.givers:
         n, attempts = 0, 0
-        while (n<1) and (attempts<10):
+        while (n < min_num_links) and (attempts < 10):
             attempts = attempts + 1
-            tr2 = traders[rng.randint(len(traders))]
+            tr2 = world.givers[rng.randint(len(world.givers))]
             isNewEdge = (tr1.add_neighbour(tr2) and tr2.add_neighbour(tr1))
             if isNewEdge:
-                edge_ends_list.append((tr1.name, tr2.name))
-                edge_labels.append(tr1.name + ' and ' + tr2.name)
                 n = n + 1
     
     H = nx.frucht_graph()  # nice to use this one or other standards...
@@ -213,14 +247,27 @@ if __name__ == '__main__':
     #tr = traders[rng.randint(len(traders))]
     #tr.do_one_gift()
 
-    for tr in traders: tr.display()
+    for tr in world.givers: 
+        tr.display()
     print '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
-    do_one_season(traders, verbose=False)
-    for tr in traders: tr.display()
+    world.do_one_season(verbose=False)
+    for tr in world.givers: 
+        tr.display()
     
 
     #%% Show the whole network as a picture    
-    G, node_posns = generate_drawable_graph(names, edge_ends_list)
+    node_labels = []
+    edge_ends_list = []
+    edge_labels = []
+    for tr1 in world.givers:
+        node_labels.append(tr1.name)
+        for tr2 in tr1.neighbours:
+            # is it in the set already?
+            if (tr2.name, tr1.name) not in edge_ends_list:
+                edge_ends_list.append((tr1.name, tr2.name))
+                edge_labels.append(tr1.name + ' and ' + tr2.name)
+                
+    G, node_posns = generate_drawable_graph(node_labels, edge_ends_list)
     edge_thck = 6
     #edge_thck = []
     #for (name1,name2) in edge_ends_list:
--- code/pair_test.py
+++ code/pair_test.py
@@ -10,46 +10,75 @@ import numpy.random as rng
 import networkx as nx
 np.set_printoptions(precision=2)
 from givers import Giver, World
+import pylab as pl
 
+def run_two_givers(g1, g2, num_gifts, dynamics='alt'):
 
-def run_two_givers(gvr1, gvr2, num_gifts, dynamics='alt'):
     if dynamics == 'alt':
-        # pick a giver at random
-        if rng.random() > 0.5:
-            g1, g2 = gvr1, gvr2
-        else:
-            g1, g2 = gvr2, gvr1
-            
-        # g1 does a gifting!
+                    
         for t in range(num_gifts):
-            g1.do_one_gift(verbose=False)
-            g2.do_one_gift(verbose=False)
-        
+            # g1 does a gifting!
+            g1.do_one_gift(temperature=1.0, verbose=False)
+            g1.append_trail()
+            g2.append_trail()
+            # g2 does a gifting!
+            g2.do_one_gift(temperature=1.0, verbose=False)
+            g2.append_trail()
+            g1.append_trail()
+            
     else: 
         print 'Do not know what dynamics to use'
     
     
-    
-    
+import sys
 if __name__ == '__main__':
-    
+    print sys.argv
+    w0 = -10.0 # -ve means never wants to PASS
+    w1 = 20.0   # +ve means gives away least valuable first
+    w2 = -6.0   # wants to be "in debt".
+    if len(sys.argv) > 1:
+        w0 = float(sys.argv[1])
+        w1 = float(sys.argv[2])
+        w2 = float(sys.argv[3])
+    print 'Using these weights for both parties: ', w0, w1, w2
+
     world = World(commodities=['love', 'respect', 'backrub'])
-    
-    g1 = Giver(world, 'Jim') #, np.array([1,1,1]))
-    g2 = Giver(world, 'Bob')
+
+    g1 = Giver(world, 'Jim', start_counts={'love':40, 'respect':20, 'backrub':0})
+    g2 = Giver(world, 'Bob', start_counts={'love':0, 'respect':20, 'backrub':40})
+
     g1.add_neighbour(g2)
     g2.add_neighbour(g1)
     world.add_node(g1)
     world.add_node(g2)
-    
-    w0, w1, w2 = 0.0, 1.0, 5.0
+
     g1.set_weights(w0, w1, w2)
     g2.set_weights(w0, w1, w2)
     
-    g1.display()
-    g2.display()
     print 'Running some gifting here'
-    run_two_givers(g1, g2, 30)
-    g1.display()
-    g2.display()
-        
+    run_two_givers(g1, g2, 12)
+
+    
+    # Display the hell out of it
+    pl.clf()
+    pl.subplot(3,1,1)
+    pl.plot(g1.utility_trail,'s-k', g2.utility_trail,'o-k')
+    pl.title('utilities over time')
+    big1 = np.max(g1.utility_trail)
+    big2 = np.max(g2.utility_trail)
+    biggest = max(big1, big2)
+    pl.gca().set_ylim(-0.5, biggest + .5)
+    pl.subplot(3,1,2)
+    for x in world.commodities:
+        pl.plot(g1.counts_trail[x],'-s', alpha=.5, label = x)
+    pl.gca().set_ylim(-0.5,40)
+    pl.xlabel(g1.name)
+    pl.gca().set_xticks([])
+    l = pl.legend()
+    pl.subplot(3,1,3)
+    for x in world.commodities:
+        pl.plot(g2.counts_trail[x],'-o', alpha=.5, label = x)
+    pl.gca().set_ylim(-0.5,40)
+    pl.xlabel(g2.name)
+    pl.gca().set_xticks([])
+    pl.savefig('apair.png')
