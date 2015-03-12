3d95e26c0ed6e935c3209dab7f0d0947d58ed027
marcus frean <marcus@ecs.vuw.ac.nz>
2015-03-12 16:59:29 +1300

    changes to pair_test

--- code/pair_test.py
+++ code/pair_test.py
@@ -11,11 +11,14 @@ import networkx as nx
 np.set_printoptions(precision=2)
 from givers import Giver, World
 import pylab as pl
+import sys
 
 def run_two_givers(g1, g2, num_gifts, dynamics='alt'):
 
     if dynamics == 'alt':
-                    
+        g1.utility_trail = []
+        g2.utility_trail = []
+        
         for t in range(num_gifts):
             # g1 does a gifting!
             g1.do_one_gift(temperature=1.0, verbose=False)
@@ -25,12 +28,38 @@ def run_two_givers(g1, g2, num_gifts, dynamics='alt'):
             g2.do_one_gift(temperature=1.0, verbose=False)
             g2.append_trail()
             g1.append_trail()
-            
+        return g1.utility_trail[-1], g2.utility_trail[-1]
     else: 
-        print 'Do not know what dynamics to use'
+        sys.exit('Do not know what dynamics to use')
     
+def display_pair_sequence(g1, g2):
+    # Display the hell out of it
+    pl.clf()
+    pl.subplot(3,1,1)
+    pl.plot(g1.utility_trail,'s-k', markerfacecolor='white')
+    pl.plot(g2.utility_trail,'o-k');
+    pl.title('utilities over time')
+    big1 = np.max(g1.utility_trail)
+    big2 = np.max(g2.utility_trail)
+    biggest = max(big1, big2)
+    pl.gca().set_ylim(-0.5, biggest + .5)
+    pl.subplot(3,1,2)
+    for x in world.commodities:
+        pl.plot(g1.counts_trail[x],'-s', alpha=.5, label = x)
+    pl.gca().set_ylim(-0.5,40)
+    pl.ylabel(g1.name)
+    pl.gca().set_xticks([])
+    l = pl.legend()
+    pl.subplot(3,1,3)
+    for x in world.commodities:
+        pl.plot(g2.counts_trail[x],'-o', alpha=.5, label = x)
+    pl.gca().set_ylim(-0.5,40)
+    pl.ylabel(g2.name)
+    pl.gca().set_xticks([])
+    pl.savefig('apair.png')
     
-import sys
+
+
 if __name__ == '__main__':
     print sys.argv
     w0 = -10.0 # -ve means never wants to PASS
@@ -40,7 +69,7 @@ if __name__ == '__main__':
         w0 = float(sys.argv[1])
         w1 = float(sys.argv[2])
         w2 = float(sys.argv[3])
-    print 'Using these weights for both parties: ', w0, w1, w2
+    print 'Using these weights for Agent #1: ', w0, w1, w2
 
     world = World(commodities=['love', 'respect', 'backrub'])
 
@@ -53,32 +82,63 @@ if __name__ == '__main__':
     world.add_node(g2)
 
     g1.set_weights(w0, w1, w2)
-    g2.set_weights(w0, w1, w2)
     
-    print 'Running some gifting here'
-    run_two_givers(g1, g2, 12)
+    
+    g2w1 = np.linspace(-5., 5, 21)
+    g2w2 = np.linspace(-5., 5, 21)
+    X,Y = np.meshgrid(g2w1,g2w2)
+    finalUtil_1 = np.zeros(shape=X.shape)
+    finalUtil_2 = np.zeros(shape=X.shape)
+    for i1,val1 in enumerate(g2w1):
+        for i2,val2 in enumerate(g2w2):
+            g2.set_weights(w0, val1, val2)  # NOTE: uses same w0 as Agent 1.
+            g1.set_count('love', 20)        
+            g1.set_count('respect', 10)        
+            g1.set_count('backrub', 0)        
+            g2.set_count('love', 0)        
+            g2.set_count('respect', 10)        
+            g2.set_count('backrub', 20)        
+            
+            u1, u2 = run_two_givers(g1, g2, 120)
+            finalUtil_1[i1,i2] = u1
+            finalUtil_2[i1,i2] = u2
 
+    # show the results as heat-maps or contours
+    fig = pl.figure()   
+    displayUtil = finalUtil_2 # - finalUtil_1
+    im = pl.imshow(displayUtil, interpolation='bilinear', origin='lower', cmap=pl.cm.Spectral, extent=(-5,5,-5,5))
+    pl.gca().set_title('Abs util vs (w0=%.1f, w1=%.1f, w2=%.1f)' % (w0, w1, w2))
+    pl.gca().set_ylabel('w1 of agent 2')
+    pl.gca().set_xlabel('w2 of agent 2')
+    levels = np.arange(np.min(displayUtil), np.max(displayUtil), 0.5)
+    #CS = pl.contour(displayUtil, levels, origin='lower',
+    #                         linewidths=2, extent=(-5,5,-5,5))
+    #CB = pl.colorbar(CS, shrink=0.8, extend='both') #colorbar for the contours
+    # We can still add a colorbar for the image, too.
+    CBI = pl.colorbar(im, orientation='vertical', shrink=0.8, extend='both')
+    pl.savefig('utility-difference.png',dpi=200)
     
-    # Display the hell out of it
-    pl.clf()
-    pl.subplot(3,1,1)
-    pl.plot(g1.utility_trail,'s-k', g2.utility_trail,'o-k')
-    pl.title('utilities over time')
-    big1 = np.max(g1.utility_trail)
-    big2 = np.max(g2.utility_trail)
-    biggest = max(big1, big2)
-    pl.gca().set_ylim(-0.5, biggest + .5)
-    pl.subplot(3,1,2)
-    for x in world.commodities:
-        pl.plot(g1.counts_trail[x],'-s', alpha=.5, label = x)
-    pl.gca().set_ylim(-0.5,40)
-    pl.xlabel(g1.name)
-    pl.gca().set_xticks([])
-    l = pl.legend()
-    pl.subplot(3,1,3)
-    for x in world.commodities:
-        pl.plot(g2.counts_trail[x],'-o', alpha=.5, label = x)
-    pl.gca().set_ylim(-0.5,40)
-    pl.xlabel(g2.name)
-    pl.gca().set_xticks([])
-    pl.savefig('apair.png')
+    """
+    fig2 = pl.figure()
+    im2 = pl.imshow(finalUtil_2, interpolation='bilinear', origin='lower', 
+                     cmap=pl.cm.gray, extent=(-5,5,-5,5))
+    pl.gca().set_title('Utility of agent 2')
+    pl.gca().set_ylabel('w1 of agent 2')
+    pl.gca().set_xlabel('w2 of agent 2')
+    levels2 = np.arange(0.0, np.max(finalUtil_2), 0.5)
+    CS2 = pl.contour(finalUtil_2, levels2, origin='lower',
+                             linewidths=2, extent=(-5,5,-5,5))
+    CB2 = pl.colorbar(CS2, shrink=0.8) #colorbar for the contours
+    # We can still add a colorbar for the image, too.
+    CBI2 = pl.colorbar(im2, orientation='vertical', shrink=0.8, extend='both')
+    pl.savefig('util2.png',dpi=200)
+    """
+    
+    
+    
+    """
+    print 'Running some gifting here'
+    final_util1, final_util_2 = run_two_givers(g1, g2, 120)
+    print final_util1, final_util_2    
+    display_pair_sequence(g1, g2)
+    """
