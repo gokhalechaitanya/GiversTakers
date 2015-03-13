fe13e5e80ef621d77fe2cc1732bad4c2a52b86b2
marcus frean <marcus@ecs.vuw.ac.nz>
2015-03-14 09:41:43 +1300

    pair_test improvements

--- code/pair_test.py
+++ code/pair_test.py
@@ -61,72 +61,79 @@ def display_pair_sequence(g1, g2):
 
 
 if __name__ == '__main__':
-    print sys.argv
-    w0 = -10.0 # -ve means never wants to PASS
-    w1 = 20.0   # +ve means gives away least valuable first
-    w2 = -6.0   # wants to be "in debt".
-    if len(sys.argv) > 1:
-        w0 = float(sys.argv[1])
-        w1 = float(sys.argv[2])
-        w2 = float(sys.argv[3])
-    print 'Using these weights for Agent #1: ', w0, w1, w2
 
     world = World(commodities=['love', 'respect', 'backrub'])
-
     g1 = Giver(world, 'Jim', start_counts={'love':40, 'respect':20, 'backrub':0})
     g2 = Giver(world, 'Bob', start_counts={'love':0, 'respect':20, 'backrub':40})
-
     g1.add_neighbour(g2)
     g2.add_neighbour(g1)
     world.add_node(g1)
     world.add_node(g2)
 
-    g1.set_weights(w0, w1, w2)
-    g2.set_weights(w0, w1, w2)
-    
     """
-    g2w1 = np.linspace(-5., 5, 21)
-    g2w2 = np.linspace(-5., 5, 21)
-    X,Y = np.meshgrid(g2w1,g2w2)
-    finalUtil_1 = np.zeros(shape=X.shape)
-    finalUtil_2 = np.zeros(shape=X.shape)
-    for i1,val1 in enumerate(g2w1):
-        for i2,val2 in enumerate(g2w2):
-            g2.set_weights(w0, val1, val2)  # NOTE: uses same w0 as Agent 1.
-            g1.set_count('love', 20)        
-            g1.set_count('respect', 10)        
-            g1.set_count('backrub', 0)        
-            g2.set_count('love', 0)        
-            g2.set_count('respect', 10)        
-            g2.set_count('backrub', 20)        
+    w0 = 10.0    # if +ve, agent never wants to PASS
+    w1 = 100.0  # if +ve, agent gives away least valuable first
+    w2 = 6.0      # if +ve, agent wants to give when other has given more.
+    """
+    if len(sys.argv) < 4:
+        sys.exit('usage: python %s  w0 w1 w2 for giver A  [w0 w1 w2 for giver B]' % (sys.argv[1]))
+    elif len(sys.argv) >= 4:
+        SINGLE_TEST = True
+        g1w0 = float(sys.argv[1])
+        g1w1 = float(sys.argv[2])
+        g1w2 = float(sys.argv[3])
+        g1.set_weights(g1w0, g1w1, g1w2)
+        g1.describe()
+    if len(sys.argv) == 7:
+        SINGLE_TEST = False
+        g2w0 = float(sys.argv[1])
+        g2w1 = float(sys.argv[2])
+        g2w2 = float(sys.argv[3])
+        g2.set_weights(g2w0, g2w1, g2w2)
+        g2.describe()
+
+    
+    if SINGLE_TEST == True:   
+        # Do a single run, with those two agents using the supplied weight values.
+        print 'Running some gifting here'
+        final_util1, final_util_2 = run_two_givers(g1, g2, 120)
+        print final_util1, final_util_2    
+        display_pair_sequence(g1, g2)
+
+    if SINGLE_TEST == False:   
+        # we will test all sorts of weights for the second player.
+        g2w1 = np.linspace(-5., 5, 21)
+        g2w2 = np.linspace(-5., 5, 21)
+        X,Y = np.meshgrid(g2w1,g2w2)
+        finalUtil_1 = np.zeros(shape=X.shape)
+        finalUtil_2 = np.zeros(shape=X.shape)
+        for i1,val1 in enumerate(g2w1):
+            for i2,val2 in enumerate(g2w2):
+                g2.set_weights(w0, val1, val2)  # NOTE: uses same w0 as Agent 1.
+                g1.set_count('love', 20)        
+                g1.set_count('respect', 10)        
+                g1.set_count('backrub', 0)        
+                g2.set_count('love', 0)        
+                g2.set_count('respect', 10)        
+                g2.set_count('backrub', 20)        
             
-            u1, u2 = run_two_givers(g1, g2, 120)
-            finalUtil_1[i1,i2] = u1
-            finalUtil_2[i1,i2] = u2
+                u1, u2 = run_two_givers(g1, g2, 120)
+                finalUtil_1[i1,i2] = u1
+                finalUtil_2[i1,i2] = u2
 
-    # show the results as heat-maps or contours
-    fig = pl.figure()   
-    displayUtil = finalUtil_2 # - finalUtil_1
-    im = pl.imshow(displayUtil, interpolation='bilinear', origin='lower', cmap=pl.cm.Spectral, extent=(-5,5,-5,5))
-    pl.gca().set_title('Abs util vs (w0=%.1f, w1=%.1f, w2=%.1f)' % (w0, w1, w2))
-    pl.gca().set_ylabel('w1 of agent 2')
-    pl.gca().set_xlabel('w2 of agent 2')
-    levels = np.arange(np.min(displayUtil), np.max(displayUtil), 0.5)
-    #CS = pl.contour(displayUtil, levels, origin='lower',
-    #                         linewidths=2, extent=(-5,5,-5,5))
-    #CB = pl.colorbar(CS, shrink=0.8, extend='both') #colorbar for the contours
-    # We can still add a colorbar for the image, too.
-    CBI = pl.colorbar(im, orientation='vertical', shrink=0.8, extend='both')
-    pl.savefig('utility-difference.png',dpi=200)
+        # show the results as heat-maps or contours
+        fig = pl.figure()   
+        displayUtil = finalUtil_2 # - finalUtil_1
+        im = pl.imshow(displayUtil, interpolation='bilinear', origin='lower', cmap=pl.cm.Spectral, extent=(-5,5,-5,5))
+        pl.gca().set_title('Abs util vs (w0=%.1f, w1=%.1f, w2=%.1f)' % (w0, w1, w2))
+        pl.gca().set_ylabel('w1 of agent 2')
+        pl.gca().set_xlabel('w2 of agent 2')
+        levels = np.arange(np.min(displayUtil), np.max(displayUtil), 0.5)
+        #CS = pl.contour(displayUtil, levels, origin='lower',
+        #                         linewidths=2, extent=(-5,5,-5,5))
+        #CB = pl.colorbar(CS, shrink=0.8, extend='both') #colorbar for the contours
+        # We can still add a colorbar for the image, too.
+        CBI = pl.colorbar(im, orientation='vertical', shrink=0.8, extend='both')
+        pl.savefig('utility-difference.png',dpi=200)
     
-    """
-
-    
-    
-    
-
-    print 'Running some gifting here'
-    final_util1, final_util_2 = run_two_givers(g1, g2, 120)
-    print final_util1, final_util_2    
-    display_pair_sequence(g1, g2)
 
